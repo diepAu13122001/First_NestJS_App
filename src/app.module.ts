ab709283e2478059778ffa3a40ed4ce760de25c3
diepAu13122001 <91660142+diepAu13122001@users.noreply.github.com>
2023-04-07 12:38:33 +0700

    create new app

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+
+@Module({
+  imports: [PrismaModule, UserModule, BookmarkModule],
+})
+export class AppModule {}
